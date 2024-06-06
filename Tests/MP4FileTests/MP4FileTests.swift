2e67cd5db2fb6d6043188f5ff990d2d3976798ac
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-06 17:11:16 +0300

    Test that nothing explodes

--- Tests/MP4FileTests/MP4FileTests.swift
+++ Tests/MP4FileTests/MP4FileTests.swift
@@ -1,9 +1,30 @@
+import Foundation
 import Testing
 
 @testable import MP4File
 
 @Suite struct MP4FileTests {
-  @Test func exampleTest() {
-    #expect(Bool(true))
+  @Test func mpeg4() {
+    let urls = Bundle.module.urls(forResourcesWithExtension: "MP4", subdirectory: nil)
+
+    #expect(urls != nil, "there should be test MP4 files in the test bundle")
+    #expect(urls!.count == 4)
+
+    for url in urls! {
+      let mp4 = MP4File(url)
+      #expect(mp4.children.count > 0)
+    }
+  }
+
+  @Test func quicktime() {
+    let urls = Bundle.module.urls(forResourcesWithExtension: "MOV", subdirectory: nil)
+
+    #expect(urls != nil, "there should be test MP4 files in the test bundle")
+    #expect(urls!.count == 1)
+
+    for url in urls! {
+      let mp4 = MP4File(url)
+      #expect(mp4.children.count > 0)
+    }
   }
 }
