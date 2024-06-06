54a99de154f992e893fcde7838e3771d5fa40b7c
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-06 16:50:24 +0300

    Set-up swift-testing

--- Package.swift
+++ Package.swift
@@ -4,20 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "MP4File",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MP4File",
-            targets: ["MP4File"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MP4File"),
-        .testTarget(
-            name: "MP4FileTests",
-            dependencies: ["MP4File"]),
-    ]
+  name: "MP4File",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "MP4File",
+      targets: ["MP4File"])
+
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-testing.git", branch: "main")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "MP4File"),
+    .testTarget(
+      name: "MP4FileTests",
+      dependencies: [
+        "MP4File",
+        .product(name: "Testing", package: "swift-testing"),
+      ],
+      resources: [
+        .copy("Resources")
+      ]),
+  ]
 )
