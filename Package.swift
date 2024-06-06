8eca9deee27dc5a4b5d1a6c9e61f2a9e72315154
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-06 16:11:15 +0300

    Init Swift package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MP4File",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MP4File",
+            targets: ["MP4File"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MP4File"),
+        .testTarget(
+            name: "MP4FileTests",
+            dependencies: ["MP4File"]),
+    ]
+)
