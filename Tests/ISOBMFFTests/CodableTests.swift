3722a197b6981d637072a5a0655a44c7414bd552
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-14 13:46:54 +0300

    Encode dates as ISO8601

--- Tests/ISOBMFFTests/CodableTests.swift
+++ Tests/ISOBMFFTests/CodableTests.swift
@@ -7,8 +7,11 @@ import Testing
   @Test func encodable() {
     let url = Bundle.module.url(forResource: "DJI_0007", withExtension: "MP4")!
     let isobmff = ISOBMFF(url)
+
     let encoder = JSONEncoder()
     encoder.outputFormatting = .prettyPrinted
+    encoder.dateEncodingStrategy = .iso8601
+
     let data = try! encoder.encode(isobmff)
     let json = String(data: data, encoding: .utf8)!
     #expect(json.count > 0)
