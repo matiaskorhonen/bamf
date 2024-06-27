47deed490f53162e3bce2e6262c50bcbaca89a3b
Matias Korhonen <me@matiaskorhonen.fi>
2024-06-27 14:50:25 +0300

    Add the DocC plugin

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
   targets: [
     .target(
